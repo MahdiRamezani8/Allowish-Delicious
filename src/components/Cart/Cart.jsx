ecd92df03d9cb01b3a56213ceaedf0dd1e4c0961
mahdi <ramezani86mraz@gmail.com>
2024-09-14 02:50:41 +0330

    (feat) cart page implemented (no styles)

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,25 @@
+import { useMenu } from "../../contexts/MenuContext";
+import { useNav } from "../../contexts/NavContext";
+import Header from "../Header/Header";
 import styles from "./Cart.module.css";
 function Cart() {
-  return <div></div>;
+  const { cart } = useMenu();
+  const { headerRef } = useNav();
+
+  return (
+    <>
+      <Header elRef={headerRef} />
+      <ul className={styles.main}>
+        {cart.map((item, i) => (
+          <li className="item" key={i}>
+            name: {item.name}
+            price: {item.totalprice} {item.priceUnit}s
+          </li>
+        ))}
+      </ul>
+      ;
+    </>
+  );
 }
 
 export default Cart;
